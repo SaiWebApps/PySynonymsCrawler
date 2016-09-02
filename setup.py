bb569fd60d6c37dac4d2f868184659b973cb685d
Sairam Krishnan <sairambkrishnan@gmail.com>
2016-09-02 05:37:02 -0700

    PyPi package description/submission files

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+  name = 'synonymscrawler',
+  packages = ['synonymscrawler'],
+  version = '0.3',
+  description = 'Python Synonyms Crawler Implementation',
+  author = 'Sairam Krishnan',
+  author_email = 'sairambkrishnan@gmail.com',
+  license = 'MIT',
+  install_requires = [
+  	'blockspring',
+  	'requests'
+  ],
+  url = 'https://github.com/SaiWebApps/SynonymsCrawler',
+  download_url = 'https://github.com/SaiWebApps/SynonymsCrawler/tarball/0.3',
+  keywords = ['Python', 'synonyms-crawler', 'web-scraping', 'text-parsing']
+)
