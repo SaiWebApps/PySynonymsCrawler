cf2146e8bd8a3dfe4e162bb3f19e1a6d92310b94
Sairam Krishnan <sairambkrishnan@gmail.com>
2016-09-02 15:14:19 -0700

    Didn't push version tag in previous commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'synonymscrawler',
   packages = ['synonymscrawler'],
-  version = '0.5',
+  version = '0.6',
   description = 'Python Synonyms Crawler Implementation',
   author = 'Sairam Krishnan',
   author_email = 'sairambkrishnan@gmail.com',
@@ -13,6 +13,6 @@ setup(
   	'requests'
   ],
   url = 'https://github.com/SaiWebApps/PySynonymsCrawler',
-  download_url = 'https://github.com/SaiWebApps/PySynonymsCrawler/tarball/0.5',
+  download_url = 'https://github.com/SaiWebApps/PySynonymsCrawler/tarball/0.6',
   keywords = ['Python', 'synonyms-crawler', 'web-scraping', 'text-parsing']
 )
